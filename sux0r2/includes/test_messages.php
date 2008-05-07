ad070d09c63409d73f7d0665f95e86942438ba20
Dac Chartrand <connerbw@users.noreply.github.com>
2008-05-07 21:43:48 +0000

    Update of htmLawed to fix deny_attribute parsing bug as discussed in forums

--- sux0r2/includes/test_messages.php
+++ sux0r2/includes/test_messages.php
@@ -7,7 +7,7 @@ include_once 'suxThreadedMessages.php';
 $msg = new suxThreadedMessages();
 
 $body = '
-<div style="width:800px; border: 1px solid red;">
+<div onmouseover="alert(\'hi\');" style="width:800px; border: 1px solid red;" >
 <h1>this is a test of the title (edit 2)</h1>
 <p><img src="media/logo.jpg" alt=" " hspace="5" vspace="5" width="250" height="48" align="right" /> TinyMCE is &agrave; platform ind&eacute;pendent web based Javascript HTML <strong>WYSIWYG</strong> editor c&ocirc;ntrol released as Open Source under LGPL by Moxiecode Systems AB. It has the ability to &ccedil;onvert HTML TEXTAREA fields or other HTML elements to editor instances.
 TinyMCE is very easy to integrate into other <span style="font-size: large;">Content Management Systems. </span></p>
