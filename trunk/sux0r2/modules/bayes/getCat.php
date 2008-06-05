95b59d6e4eb86d34a44b97217289d633bd0aef2b
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-06-05 20:17:22 +0000

    Bayesian module, checkpoint

---
+++ trunk/sux0r2/modules/bayes/getCat.php
@@ -0,0 +1,41 @@
+<?php
+
+// Ajax
+// Echo the categorization of a document
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once(dirname(__FILE__) . '/../../initialize.php');
+require_once(dirname(__FILE__) . '/../../includes/suxNaiveBayesian.php');
+
+// ---------------------------------------------------------------------------
+// Sanitize
+// ---------------------------------------------------------------------------
+
+if (!filter_var($_POST['id'], FILTER_VALIDATE_INT)) {
+    echo '<p>Error: Vector cannot be empty</p>';
+    exit;
+}
+
+if (!trim($_POST['document'])) {
+    echo '<p>Error: Document cannot be empty</p>';
+    exit;
+}
+
+// ---------------------------------------------------------------------------
+// Generate HTML
+// ---------------------------------------------------------------------------
+
+$nb = new suxNaiveBayesian();
+$scores = $nb->categorize($_POST['document'], $_POST['id']);
+
+$html = '<p><table border="1">';
+$html .= '<thead><tr><th>Categories</th><th>Scores</th></tr></thead><tbody>'. "\n";
+foreach ($scores as $k => $v) {
+    $html .= "<tr><td>{$k}</td><td>" . round($v*100, 2) . " %</td></tr>\n";
+}
+$html .= '</tbody></table></p>' . "\n";
+$html .= '<p><em>Categorized on: ' . date('D M j, G:i:s') . '</em></p>';
+
+echo $html;
+
+?>
