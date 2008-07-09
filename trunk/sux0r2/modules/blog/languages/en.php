d4af1c23776b66a4db6c89c01dc75f6720f5508a
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-07-09 21:03:19 +0000

    Changed text

--- trunk/sux0r2/modules/blog/languages/en.php
+++ trunk/sux0r2/modules/blog/languages/en.php
@@ -6,6 +6,7 @@ $gtext['cancel'] = 'Cancel';
 $gtext['form_error'] = 'The form was not submitted, see errors below';
 $gtext['form_problem'] = 'The form was not submitted, please try again';
 $gtext['submit'] = 'Submit';
+$gtext['linked_to'] = 'Linked to the following Bayes documents';
 
 
 ?>
