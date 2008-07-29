6702f19a6545dab861c668e01357e8fe1ce6f7ff
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-07-29 21:50:35 +0000

    Upload photos, work in progress

--- trunk/sux0r2/includes/suxPhoto.php
+++ trunk/sux0r2/includes/suxPhoto.php
@@ -25,30 +25,30 @@
 */
 
 class suxPhoto {
-    
+
     // Database stuff
     protected $db;
     protected $inTransaction = false;
     protected $db_photos = 'photos';
     protected $db_albums = 'photoalbums';
     protected $db_driver; // database type
-    
-    
+
+
     /**
     * Constructor
     */
     function __construct() {
-        
+
     	$this->db = suxDB::get();
         $this->db_driver = $this->db->getAttribute(PDO::ATTR_DRIVER_NAME);
         set_exception_handler(array($this, 'exceptionHandler'));
-        
+
     }
-    
+
     // ------------------------------------------------------------------------
-    // Photoalbum accessors
+    // Photoalbum functions
     // ------------------------------------------------------------------------
-    
+
     /**
     * Get an album by id
     *
@@ -57,10 +57,10 @@ class suxPhoto {
     * @return array|false
     */
     function getAlbum($id, $unpub = false) {
-        
+
         // Sanity check
         if (!filter_var($id, FILTER_VALIDATE_INT)) throw new Exception('Invalid album id');
-        
+
         $query = "SELECT * FROM {$this->db_albums} WHERE id = ? ";
         if (!$unpub) {
             // Only show published items
@@ -74,18 +74,18 @@ class suxPhoto {
             }
         }
         $query .= 'LIMIT 1 ';
-        
-        
+
+
         $st = $this->db->prepare($query);
         $st->execute(array($id));
-        
+
         $album = $st->fetch(PDO::FETCH_ASSOC);
         if ($album) return $album;
         else return false;
-        
-        
-    } 
-    
+
+
+    }
+
     /**
     * Saves an album to the database
     *
@@ -95,38 +95,38 @@ class suxPhoto {
     * @return int insert id
     */
     function saveAlbum($users_id, array $album, $trusted = -1) {
-        
+
         // -------------------------------------------------------------------
         // Sanitize
         // -------------------------------------------------------------------
-        
+
         if (!filter_var($users_id, FILTER_VALIDATE_INT) || $users_id <= 0) throw new Exception('Invalid user id');
         if (!isset($album['title']) || !isset($album['body'])) throw new Exception('Invalid $album array');
-        
+
         // Album id
         if (isset($album['id'])) {
             if (!filter_var($album['id'], FILTER_VALIDATE_INT) || $album['id'] <= 0) throw new Exception('Invalid album id');
             else $clean['id'] = $album['id'];
         }
-        
+
         // Users id
         $clean['users_id'] = $users_id;
-        
+
         // No HTML in title
         $clean['title'] = strip_tags($album['title']);
-        
+
         // Sanitize HTML in body
         $clean['body_html'] = suxFunct::sanitizeHtml($album['body'], $trusted);
-        
+
         // Convert and copy body to UTF-8 plaintext
         require_once(dirname(__FILE__) . '/suxHtml2UTF8.php');
         $converter = new suxHtml2UTF8($clean['body_html']);
         $clean['body_plaintext']  = $converter->getText();
-        
+
         // Draft, boolean / tinyint
         $clean['draft'] = 0;
         if (isset($album['draft'])) $clean['draft'] = 1;
-        
+
         // Publish date
         if (isset($album['published_on'])) {
             // ISO 8601 date format
@@ -135,45 +135,141 @@ class suxPhoto {
             if (!preg_match($regex, $album['published_on'])) throw new Exception('Invalid date');
             $clean['published_on'] = $album['published_on'];
         }
-        else $clean['published_on'] = date('c');  
-        
-        
+        else $clean['published_on'] = date('c');
+
+
         // We now have the $clean[] array
-        
+
         // --------------------------------------------------------------------
         // Go!
         // --------------------------------------------------------------------
-        
-        
+
+
         if (isset($clean['id'])) {
-            
+
             // UPDATE
             $query = suxDB::prepareUpdateQuery($this->db_albums, $clean);
             $st = $this->db->prepare($query);
             $st->execute($clean);
             $id = $clean['id'];
-            
+
         }
         else {
-            
+
             // INSERT
             $query = suxDB::prepareInsertQuery($this->db_albums, $clean);
             $st = $this->db->prepare($query);
             $st->execute($clean);
             $id = $this->db->lastInsertId();
-            
+
         }
-        
+
         return $id;
-        
-    }   
-    
-    
-    
+
+    }
+
+
     // ------------------------------------------------------------------------
-    // Static Image Functions
+    // Photo functions
     // ------------------------------------------------------------------------
-    
+
+
+    /**
+    * Check if the photo is already in this album
+    *
+    * @param string $md5
+    * @param int $users_id
+    * @param int $photoalbums_id
+    * @return bool
+    */
+    function isDupe($md5, $users_id, $photoalbums_id) {
+
+        $q = "SELECT COUNT(*) FROM {$this->db_photos} WHERE md5 = ? AND users_id = ? AND photoalbums_id = ? LIMIT 1 ";
+        $st = $this->db->prepare($q);
+        $st->execute(array($md5, $users_id, $photoalbums_id));
+        if ($st->fetchColumn() <= 0) return false;
+        else return true;
+
+    }
+
+
+    /**
+    * Saves a photo to the database
+    *
+    * @param int $users_id users_id
+    * @param array $album required keys => (photoalbums_id, image, md5) optional keys => (body)
+    * @param int $trusted passed on to sanitizeHtml()
+    * @return int insert id
+    */
+    function savePhoto($users_id, array $photo, $trusted = -1) {
+
+        // -------------------------------------------------------------------
+        // Sanitize
+        // -------------------------------------------------------------------
+
+        if (!filter_var($users_id, FILTER_VALIDATE_INT) || $users_id <= 0) throw new Exception('Invalid user id');
+        if (!isset($photo['photoalbums_id']) || !isset($photo['image']) || !isset($photo['md5'])) throw new Exception('Invalid $photo array');
+
+        // photo id
+        if (isset($photo['id'])) {
+            if (!filter_var($photo['id'], FILTER_VALIDATE_INT) || $photo['id'] <= 0) throw new Exception('Invalid photo id');
+            else $clean['id'] = $photo['id'];
+        }
+
+        // Begin collecting $clean array
+        $clean['users_id'] = $users_id;
+        $clean['image'] = $photo['image'];
+        $clean['photoalbums_id'] = $photo['photoalbums_id'];
+        $clean['md5'] = $photo['md5'];
+
+        // Set an empty string if empty
+        if (!isset($album['body'])) $album['body'] = '';
+
+        // Sanitize HTML in body
+        $clean['body_html'] = suxFunct::sanitizeHtml($album['body'], $trusted);
+
+        // Convert and copy body to UTF-8 plaintext
+        require_once(dirname(__FILE__) . '/suxHtml2UTF8.php');
+        $converter = new suxHtml2UTF8($clean['body_html']);
+        $clean['body_plaintext']  = $converter->getText();
+
+
+        // We now have the $clean[] array
+
+        // --------------------------------------------------------------------
+        // Go!
+        // --------------------------------------------------------------------
+
+
+        if (isset($clean['id'])) {
+
+            // UPDATE
+            $query = suxDB::prepareUpdateQuery($this->db_photos, $clean);
+            $st = $this->db->prepare($query);
+            $st->execute($clean);
+            $id = $clean['id'];
+
+        }
+        else {
+
+            // INSERT
+            $query = suxDB::prepareInsertQuery($this->db_photos, $clean);
+            $st = $this->db->prepare($query);
+            $st->execute($clean);
+            $id = $this->db->lastInsertId();
+
+        }
+
+        return $id;
+
+    }
+
+
+
+    // ------------------------------------------------------------------------
+    // Image Functions
+    // ------------------------------------------------------------------------
+
     /**
     * Assign a unique id to an image file name. Returns an array of filename
     * conventions
@@ -182,21 +278,21 @@ class suxPhoto {
     * @return array
     */
     static function renameImage($filename) {
-        
+
         $pattern = '/(\.jpe?g|\.gif|\.png)$/i';
         $uniqid = time() . substr(md5(microtime()), 0, rand(5, 12));
-        
+
         $replacement = "_{$uniqid}" . "$1";
         $resize = preg_replace($pattern, $replacement, $filename);
-        
+
         $replacement = "_{$uniqid}_fullsize" . "$1";
         $fullsize = preg_replace($pattern, $replacement, $filename);
-        
+
         return array($resize, $fullsize);
-        
+
     }
-    
-    
+
+
     /**
     * Convert thumbail filename to fullsize filename
     * i.e. filename_12345.jpg to filename_12345_fullsize.jpg
@@ -205,15 +301,15 @@ class suxPhoto {
     * @return string
     */
     static function t2fImage($filename) {
-        
+
         $pattern = '/(\.jpe?g|\.gif|\.png)$/i';
         $replacement = '_fullsize' . "$1";
         $fullsize = preg_replace($pattern, $replacement, $filename);
         return $fullsize;
-        
+
     }
-    
-    
+
+
     /**
     * Proprtionally crop and resize an image file
     *
@@ -227,17 +323,17 @@ class suxPhoto {
     * @param int $blue RGB blue for imagecolorallocate()
     */
     static function resizeImage($format, $filein, $fileout, $imagethumbsize_w, $imagethumbsize_h, $red = 255, $green = 255, $blue = 255) {
-        
+
         $format = strtolower($format);
         if ($format == 'jpg') $format = 'jpeg'; // fix stupid mistake
         if (!($format == 'jpeg' || $format == 'gif' || $format == 'png'))  {
             throw new Exception('Invalid image format');
         }
-        
+
         /* Try to avoid problems with memory limit */
-        
+
         $size = getimagesize($filein);
-        
+
         if ($format == 'jpeg') {
             // Jpeg
             $fudge = 1.65; // This is a guestimate, your mileage may very
@@ -249,50 +345,50 @@ class suxPhoto {
             if (isset($size['bits'])) $memoryNeeded = $memoryNeeded * $size['bits'];
             $memoryNeeded = round($memoryNeeded);
         }
-        
+
         if (memory_get_usage() + $memoryNeeded > (int) ini_get('memory_limit') * pow(1024, 2)) {
             trigger_error('Image is too big, attempting to compensate...', E_USER_WARNING);
             ini_set('memory_limit', (int) ini_get('memory_limit') + ceil(((memory_get_usage() + $memoryNeeded) - (int) ini_get('memory_limit') * pow(1024, 2)) / pow(1024, 2)) . 'M');
         }
-        
+
         /* Proceed with resizing */
-        
+
         $func = 'imagecreatefrom' . $format;
         $image = $func($filein);
         if (!$image) throw new Exception('Invalid image format');
-        
+
         $width = $imagethumbsize_w;
         $height = $imagethumbsize_h;
         list($width_orig, $height_orig) = getimagesize($filein);
-        
+
         if ($width_orig < $height_orig) {
             $height = ($imagethumbsize_w / $width_orig) * $height_orig;
         }
         else {
             $width = ($imagethumbsize_h / $height_orig) * $width_orig;
         }
-        
+
         //if the width is smaller than supplied thumbnail size
         if ($width < $imagethumbsize_w) {
             $width = $imagethumbsize_w;
             $height = ($imagethumbsize_w/ $width_orig) * $height_orig;
         }
-        
+
         //if the height is smaller than supplied thumbnail size
         if ($height < $imagethumbsize_h) {
             $height = $imagethumbsize_h;
             $width = ($imagethumbsize_h / $height_orig) * $width_orig;
         }
-        
+
         // Original, proportionally modified
         $thumb = imagecreatetruecolor($width, $height);
         $bgcolor = imagecolorallocate($thumb, $red, $green, $blue);
         ImageFilledRectangle($thumb, 0, 0, $width, $height, $bgcolor);
         imagealphablending($thumb, true);
         imagecopyresampled($thumb, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-        
+
         imagedestroy($image); // Free memory
-        
+
         // Thumbnail
         $thumb2 = imagecreatetruecolor($imagethumbsize_w, $imagethumbsize_h);
         $white = imagecolorallocate($thumb2, 255, 255, 255);
@@ -301,37 +397,37 @@ class suxPhoto {
         $w1 = ($width/2) - ($imagethumbsize_w/2);
         $h1 = ($height/2) - ($imagethumbsize_h/2);
         imagecopyresampled($thumb2, $thumb, 0, 0, $w1, $h1, $imagethumbsize_w , $imagethumbsize_h ,$imagethumbsize_w, $imagethumbsize_h);
-        
+
         imagedestroy($thumb); // Free memory
-        
+
         $func = 'image' . $format;
         $func($thumb2, $fileout);
-        
+
         imagedestroy($thumb2); // Free memory
-        
+
     }
-    
-    
+
+
     // ----------------------------------------------------------------------------
     // Exception Handler
     // ----------------------------------------------------------------------------
-    
-    
+
+
     /**
     * @param Exception $e an Exception class
     */
     function exceptionHandler(Exception $e) {
-        
+
         if ($this->db && $this->inTransaction) {
             $this->db->rollback();
             $this->inTransaction = false;
         }
-        
+
         throw($e); // Hot potato!
-        
+
     }
-    
-    
+
+
 }
 
 ?>
---
+++ trunk/sux0r2/templates/sux0r/photos/upload.tpl
@@ -0,0 +1,72 @@
+{include file=$r->xhtml_header}
+
+<div id="proselytizer">
+
+{* Header *}
+<div id="header">
+    {insert name="userInfo"}
+</div>
+<div class="clearboth"></div>
+
+{* Content *}
+<div id="middle">
+
+<fieldset>
+<legend>Upload</legend>
+
+<form action="{$r->text.form_url}" name="default" method="post" enctype="multipart/form-data" accept-charset="utf-8" >
+<input type="hidden" name="token" value="{$token}" />
+
+{if $validate.default.is_error !== false}
+<p class="errorWarning">{$r->text.form_error} :</p>
+{elseif $r->detectPOST()}
+<p class="errorWarning">{$r->text.form_problem} :</p>
+{/if}
+
+<p>
+{strip}
+    {capture name=error2}
+    {validate id="album" message=$r->text.form_error_3}
+    {/capture}
+{/strip}
+
+<label for="album" {if $smarty.capture.error2}class="error"{/if}>{$r->text.album} :</label>
+{html_options name='album' options=$r->getAlbums() selected=$album}
+{$smarty.capture.error2}
+</p>
+
+
+<p>
+{strip}
+    {capture name=error1}
+    {validate id="image" message=$r->text.form_error_1}
+    {validate id="image2" message=$r->text.form_error_2}
+    {/capture}
+{/strip}
+
+<label for="image" {if $smarty.capture.error1}class="error"{/if}>{$r->text.upload} :</label>
+<input type="file" name="image" class="imageFile" />
+{$smarty.capture.error1}
+</p>
+
+<p>
+{$r->text.max_filesize}: {$r->text.upload_max_filesize}<br />
+{$r->text.extensions}: {$r->text.supported}
+</p>
+
+
+
+<p>
+<input type="button" class="button" value="{$r->text.cancel}" onclick="document.location='{$r->text.back_url}';" />
+<input type="submit" value="{$r->text.submit}" class="button" />
+</p>
+
+</form>
+
+</fieldset>
+
+</div>
+
+</div>
+
+{include file=$r->xhtml_footer}
