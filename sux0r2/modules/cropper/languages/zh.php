e9a8d2b9097c97d37cda7540ea2734398c2b47c2
Dac Chartrand <connerbw@users.noreply.github.com>
2008-10-06 17:26:48 +0000

    Chinese translations (cn_ZN)

---
+++ sux0r2/modules/cropper/languages/zh.php
@@ -0,0 +1,15 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['cancel'] = 'åæ¶';
+$gtext['cancelled'] = 'å·²åæ¶!';
+$gtext['crop_image'] = 'åªåå¾ç';
+$gtext['form_error'] = 'è¡¨åæªè¢«æäº¤ï¼æ³¨æåºéé¡¹ç®';
+$gtext['form_problem'] = 'è¡¨åæªè¢«æäº¤ï¼è¯·éè¯';
+$gtext['height'] = 'é«åº¦';
+$gtext['submit'] = 'æäº¤';
+$gtext['width'] = 'å®½åº¦';
+
+?>
---
+++ sux0r2/modules/feeds/languages/zh.php
@@ -0,0 +1,44 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['actions'] = 'æä½';
+$gtext['admin'] = 'Feedsç®¡ç';
+$gtext['alert_delete'] = 'ç¡®è®¤å é¤è¿äºfeeds? å¯¹åºæ¡ç®ä¹ä¼è¢«å é¤ã';
+$gtext['approve'] = 'å®¡æ ¸feeds';
+$gtext['approve_2'] = 'å®¡æ ¸';
+$gtext['back'] = 'è¿å';
+$gtext['back_2'] = 'è¿åå°Feeds';
+$gtext['body'] = 'æ­£æ';
+$gtext['cancel'] = 'åæ¶';
+$gtext['delete'] = 'å é¤';
+$gtext['draft'] = 'èç¨¿';
+$gtext['edit'] = 'ç¼è¾';
+$gtext['edit_2'] = 'ç¼è¾RSS Feed';
+$gtext['feed'] = 'Feed';
+$gtext['feeds'] = 'RSS Feeds';
+$gtext['form_error'] = 'è¡¨åæªè¢«æäº¤ï¼æ³¨æåºéé¡¹ç®';
+$gtext['form_error_1'] = 'URLä¸è½ä¸ºç©º';
+$gtext['form_error_2'] = 'æ æçURL';
+$gtext['form_error_3'] = 'éå¤çRSS Feedï¼å·²è¢«æ·»å ï¼ ';
+$gtext['form_error_4'] = 'RSS Feedçèµ·æ¥æ æ';
+$gtext['form_problem'] = 'è¡¨åæªè¢«æäº¤ï¼è¯·éè¯';
+$gtext['header'] = 'Read all about it discover all the news';
+$gtext['items'] = 'æ¡ç®';
+$gtext['manage'] = 'ç®¡çfeeds';
+$gtext['new'] = 'æ°RSS Feed';
+$gtext['not_found'] = 'æ²¡ææ¾å°';
+$gtext['nothing'] = 'æ²¡æä¸è¥¿éè¦å®¡æ ¸';
+$gtext['published_on'] = 'åå¸ä¸';
+$gtext['read_more'] = 'éè¯»æ´å¤';
+$gtext['save_draft'] = 'å­ä¸ºèç¨¿';
+$gtext['submit'] = 'æäº¤';
+$gtext['success'] = 'æå!';
+$gtext['success2'] = 'ä¸ä¹å°è¢«ç®¡çåå®¡æ ¸';
+$gtext['suggest'] = 'æäº¤feed';
+$gtext['suggested'] = 'æäº¤è';
+$gtext['title'] = 'æ é¢';
+$gtext['url'] = 'URL';
+
+?>
