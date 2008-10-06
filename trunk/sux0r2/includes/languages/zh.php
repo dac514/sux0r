092b2a0f85865e1124000a885f6d635a8a4e81b2
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-10-06 17:26:48 +0000

    Chinese translations (cn_ZN)

---
+++ trunk/sux0r2/includes/languages/zh.php
@@ -0,0 +1,38 @@
+ï»¿<?php
+
+/* Navigation menu */
+
+$gtext['navcontainer'] = array(
+
+    'é¦é¡µ' => suxFunct::makeUrl('/home'),
+    'æ¥å¿' => suxFunct::makeUrl('/blog'),
+    'Feeds' => suxFunct::makeUrl('/feeds'),
+    'ä¹¦ç­¾' => suxFunct::makeUrl('/bookmarks'),
+    'å¾ç' => suxFunct::makeUrl('/photos'),
+    'æºä»£ç ' => 'http://sourceforge.net/projects/sux0r/',
+    'æå©!' => 'http://sourceforge.net/project/project_donations.php?group_id=131752',
+
+    );
+
+/* Copyright */
+
+$gtext['copyright'] = '<a href="http://www.sux0r.org/">sux0r</a> is copyright &copy;
+<a href="http://www.trotch.com/">Trotch Inc</a> ' . date('Y') . ' and is distributed under
+the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero General Public License</a>.';
+
+/* Now back our regular scheduled program */
+
+$gtext['404_continue'] = 'ç¹å»æ­¤å¤ç»§ç»­';
+$gtext['404_h1'] = 'é¢ï¼é¡µé¢æ²¡ææ¾å°ã(Error 404)';
+$gtext['404_p1'] = 'åºäºæäºåå  (éè¯¯ç±»åçURLï¼ä»å¶ä»ç½ç«è½¬å¥åºéï¼è¿æçæç´¢å¼æä¿¡æ¯æèæä»¬å äºä¸ªæä»¶)é¡µé¢ä¸å­å¨';
+$gtext['admin'] = 'ç®¡ç';
+$gtext['banned_continue'] = 'ç¹å»æ­¤å¤ç»§ç»­';
+$gtext['banned_h1'] = 'ç¦ç¨';
+$gtext['banned_p1'] = 'ä½ æ¯ä¸ªåèï¼éå¸¸éå¸¸åã';
+$gtext['continue'] = 'ç»§ç»­';
+$gtext['login'] = 'ç»å½';
+$gtext['logout'] = 'éåº';
+$gtext['register'] = 'æ³¨å';
+$gtext['welcome'] = 'æ¬¢è¿';
+
+?>
---
+++ trunk/sux0r2/modules/bookmarks/languages/zh.php
@@ -0,0 +1,54 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['admin'] = 'ä¹¦ç­¾ç®¡ç';
+$gtext['alert_delete'] = 'ç¡®è®¤å é¤è¿äºä¹¦ç­¾ï¼';
+$gtext['approve'] = 'å®¡æ ¸ä¹¦ç­¾';
+$gtext['approve_2'] = 'å®¡æ ¸';
+$gtext['back'] = 'è¿å';
+$gtext['back_2'] = 'è¿åå°ä¹¦ç­¾';
+$gtext['bayes_categories'] = 'è´å¶æ¯åç±»';
+$gtext['body'] = 'æ­£æ';
+$gtext['bookmarks'] = 'ä¹¦ç­¾';
+$gtext['cancel'] = 'åæ¶';
+$gtext['date'] = 'æ¥æ';
+$gtext['delete'] = 'å é¤';
+$gtext['draft'] = 'èç¨¿';
+$gtext['edit'] = 'ç¼è¾';
+$gtext['edit_2'] = 'ç¼è¾ä¹¦ç­¾';
+$gtext['form_error'] = 'è¡¨åæªè¢«æäº¤ï¼æ³¨æåºéé¡¹ç®';
+$gtext['form_error_1'] = 'URLä¸è½ä¸ºç©º';
+$gtext['form_error_2'] = 'æ æçURL';
+$gtext['form_error_3'] = 'éå¤çURLï¼å·²è¢«æ·»å çï¼';
+$gtext['form_error_4'] = 'URLçèµ·æ¥ä¸æ­£ç¡®';
+$gtext['form_error_5'] = 'æ é¢ä¸è½ä¸ºç©º';
+$gtext['form_error_6'] = 'æ­£æä¸è½ä¸ºç©º';
+$gtext['form_error_7'] = 'æ æçæ¥æ';
+$gtext['form_error_8'] = 'æ æçæ¶é´';
+$gtext['form_problem'] = 'è¡¨åæªè¢«æäº¤ï¼è¯·éè¯';
+$gtext['header'] = 'Shamelessness thinly veiled as reciprocity';
+$gtext['my_bookmarks'] = 'æçä¹¦ç­¾';
+$gtext['new'] = 'æ°ä¹¦ç­¾';
+$gtext['none'] = 'æ ';
+$gtext['not_found'] = 'æ²¡ææ¾å°';
+$gtext['nothing'] = 'æ²¡æä¸è¥¿éè¦å®¡æ ¸';
+$gtext['published_on'] = 'åå¸äº';
+$gtext['save_draft'] = 'å­ä¸ºèç¨¿';
+$gtext['sort_alpha'] = 'æå­å¸æåº';
+$gtext['sort_date'] = 'ææ¥ææåº';
+$gtext['submit'] = 'æäº¤';
+$gtext['success'] = 'æå';
+$gtext['success2'] = 'ä¸ä¹å°è¢«ç®¡çåå®¡æ ¸ã';
+$gtext['suggest'] = 'æäº¤ä¹¦ç­¾';
+$gtext['suggested'] = 'æä¾è';
+$gtext['tag_cloud'] = 'Tagé';
+$gtext['tag'] = 'Tag';
+$gtext['tags'] = 'Tags';
+$gtext['tags_2'] = 'Tags,åéå½ä»¤';
+$gtext['time'] = 'æ¶é´';
+$gtext['title'] = 'æ é¢';
+$gtext['url'] = 'URL';
+
+?>
