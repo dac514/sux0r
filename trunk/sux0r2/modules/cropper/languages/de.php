35ae418f5c0bb3dbe047144f8e0191881048f758
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-10-16 20:03:46 +0000

    German translation

---
+++ trunk/sux0r2/modules/cropper/languages/de.php
@@ -0,0 +1,15 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['cancel'] = 'Abbrechen';
+$gtext['cancelled'] = 'Abgebrochen!';
+$gtext['crop_image'] = 'Bild zuschneiden';
+$gtext['form_error'] = 'Das Formular wurde nicht Ã¼bertragen, siehe Fehler unten';
+$gtext['form_problem'] = 'Das Formular wurde nicht Ã¼bertragen, bitte versuchen Sie es erneut';
+$gtext['height'] = 'HÃ¶he';
+$gtext['submit'] = 'Senden';
+$gtext['width'] = 'Breite';
+
+?>
