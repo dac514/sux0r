2c002d99dcffb482fbd5bc3a292a49bb23f2ee0e
Dac Chartrand <connerbw@users.noreply.github.com>
2008-10-16 20:03:46 +0000

    German translation

---
+++ sux0r2/modules/bayes/languages/de.php
@@ -0,0 +1,72 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['add'] = 'HinzufÃ¼gen';
+$gtext['add_doc'] = 'Dokument hinzufÃ¼gen';
+$gtext['alert_cat'] = 'Sind Sie sich sicher, dass Sie diese Kategorie lÃ¶schen wollen?';
+$gtext['alert_doc'] = 'Sind Sie sich sicher, dass Sie dieses Dokument lÃ¶schen wollen?';
+$gtext['alert_unshare'] = 'Sind Sie sich sicher, dass Sie diese Vektoren nicht mehr verteilen wollen?';
+$gtext['alert_vec'] = 'Sind Sie sich sicher, dass Sie diesen Vektor lÃ¶schen wollen?';
+$gtext['back'] = 'ZurÃ¼ck';
+$gtext['back_2'] = 'ZurÃ¼ck zu meinem Profil';
+$gtext['cat_doc'] = 'Dokument kategorisieren';
+$gtext['categories'] = 'Kategorien';
+$gtext['categorize'] = 'Kategorisieren';
+$gtext['categorized_on'] = 'Kategorisiert an';
+$gtext['delete'] = 'LÃ¶schen';
+$gtext['documents'] = 'Dokumente';
+$gtext['edit_bayes'] = 'Bayessche Kategorien bearbeiten';
+$gtext['error'] = 'Fehler';
+$gtext['filter1'] = 'Filterung nach Top';
+$gtext['filter2'] = 'Filterung nach Schwelle';
+$gtext['form_error'] = 'Dieses Formular wurde nicht Ã¼bertragen, siehe Fehler unten';
+$gtext['form_error_1'] = 'Vektor kann nicht leer sein';
+$gtext['form_error_2'] = 'Kategorie kann nicht leer sein';
+$gtext['form_error_3'] = 'Vektor kann nicht leer sein';
+$gtext['form_error_4'] = 'Kategorie kann nicht leer sein';
+$gtext['form_error_5'] = 'Vektor ist zu lang';
+$gtext['form_error_6'] = 'Kategorie ist zu groÃ';
+$gtext['form_error_7'] = 'Dokument kann nicht leer sein';
+$gtext['form_error_8'] = 'Benutzer kann nicht leer sein';
+$gtext['form_error_9'] = 'UngÃ¼ltig';
+$gtext['form_error_10'] = 'Kann Vektor nicht selbst verteilen';
+$gtext['form_error_11'] = 'Benutzer existiert nicht';
+$gtext['form_error_12'] = 'Nichts war ausgewÃ¤hlt?';
+$gtext['is_linked'] = 'Dieses Dokument ist verknÃ¼pft : ';
+$gtext['is_not_linked'] = 'Dieses Dokument ist nicht verknÃ¼pft...';
+$gtext['new_cat'] = 'Neue Kategorie';
+$gtext['new_vec'] = 'Neuer Vektor';
+$gtext['owner'] = 'Besitzer';
+$gtext['owner2'] = 'Besitzer? (Wenn ausgewÃ¤hlt kann der Benutzer Dokumente hinzufÃ¼gen)';
+$gtext['remove_cat'] = 'Kategorie entfernen';
+$gtext['remove_doc'] = 'Dokument entfernen';
+$gtext['remove_vec'] = 'Vektor entfernen';
+$gtext['scores'] = 'Ergebnisse';
+$gtext['search'] = 'StichwÃ¶rter';
+$gtext['share'] = 'Verteilen';
+$gtext['share_category'] = 'Bayessche Kategorie verteilt mit';
+$gtext['share_vec'] = 'Vektor verteilen';
+$gtext['shared'] = 'Verteilt';
+$gtext['stats'] = 'Statistiken';
+$gtext['synopsis'] = 'Zusammenfassung';
+$gtext['synopsis_1'] = 'Ein Vektor ist eine Liste von Kategorien. Sie mÃ¼ssen mindestens zwei Kategorien in einem Vektor haben, um die <a href="http://de.wikipedia.org/wiki/Bayes-Klassifikator">bayessche Klassifikation</a> ausfÃ¼hren zu kÃ¶nnen.';
+$gtext['synopsis_2'] = 'Zum Beispiel kÃ¶nnte ein Vektor mit dem Namen <strong>GefÃ¼hle</strong> die Kategorien <strong>GlÃ¼cklich</strong>, <strong>BekÃ¼mmert</strong> and <strong>GeÃ¤rgert</strong> haben. Ein Vektor mit dem Namen <strong>Filter</strong> kÃ¶nnte die Kategorien <strong>Spam</strong> und <strong>Nicht-Spam</strong> haben.';
+$gtext['synopsis_3'] = 'Im Gegensatz dazu wÃ¼rden Sie nicht <strong>Spam</strong> in den <strong>GefÃ¼hle</strong>-Vektor stellen, weil sie nicht zu dieser Liste zugehÃ¶rig ist.';
+$gtext['synopsis_4'] = 'Bitte beachten Sie, dass <strong>Hunderte von Dokumenten</strong> in jeder Kategorie hinzugefÃ¼gt werden mÃ¼ssen, bevor jegliche Genauigkeit bei der Klassifikation vorhanden ist.';
+$gtext['threshold'] = 'Schwelle';
+$gtext['to'] = 'zu';
+$gtext['tokens'] = 'Token';
+$gtext['top'] = 'Anfang';
+$gtext['train'] = 'HinzufÃ¼gen';
+$gtext['trainer'] = 'Benutzer erlauben, Dokumente hinzuzufÃ¼gen?';
+$gtext['trainer'] = 'HinzufÃ¼ger';
+$gtext['unshare'] = 'Nicht mehr verteilen';
+$gtext['unshare_category'] = 'Bayessche Kategorie wird nicht mehr geteilt mit';
+$gtext['user'] = 'Benutzer';
+$gtext['vector'] = 'Vektor';
+$gtext['vectors'] = 'Vektoren';
+$gtext['with'] = 'Mit Freund';
+
+?>
---
+++ sux0r2/modules/blog/languages/de.php
@@ -0,0 +1,65 @@
+ï»¿<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['admin'] = 'Blog-Verwaltung';
+$gtext['alert_delete'] = 'Sind Sie sich sicher, dass Sie diesen Blog lÃ¶schen wollen? Kommentare werden ebenfalls gelÃ¶scht.';
+$gtext['archives'] = 'Archive';
+$gtext['author'] = 'Autor';
+$gtext['authors'] = 'Autoren';
+$gtext['back'] = 'ZurÃ¼ck zum Blog';
+$gtext['bayes_categories'] = 'Bayessche Kategorien nach Autor';
+$gtext['blog'] = 'Blog';
+$gtext['body'] = 'Hauptteil';
+$gtext['bookmarks'] = 'Lesezeichen';
+$gtext['by'] = 'Nach';
+$gtext['cancel'] = 'Abbrechen';
+$gtext['category'] = 'Kategorie';
+$gtext['comments'] = 'Kommentare';
+$gtext['date'] = 'Datum';
+$gtext['delete'] = 'LÃ¶schen';
+$gtext['draft'] = 'Entwurf';
+$gtext['edit'] = 'Bearbeiten';
+$gtext['edit_2'] = 'Blog bearbeiten';
+$gtext['error_1'] = 'Titel kann nicht leer sein';
+$gtext['error_2'] = 'Hauptteil kann nicht leer sein';
+$gtext['error_3'] = 'UngÃ¼ltiger Dateityp';
+$gtext['error_4'] = 'Datei ist zu groÃ';
+$gtext['error_5'] = 'UngÃ¼ltiges Datum';
+$gtext['error_6'] = 'UngÃ¼ltige Zeit';
+$gtext['error_7'] = 'URL ist leer';
+$gtext['error_8'] = 'URL ist ungÃ¼ltig';
+$gtext['extensions'] = 'UnterstÃ¼tzte Erweiterungen';
+$gtext['form_error'] = 'Das Formular wurde nicht Ã¼bertragen, siehe Fehler unten';
+$gtext['form_problem'] = 'Das Formular wurde nicht Ã¼bertragen, bitte versuchen Sie es erneut';
+$gtext['header'] = 'Wir bÃ¼geln unsere Hosen in das Universum';
+$gtext['image'] = 'Bild';
+$gtext['in'] = 'In';
+$gtext['linked_to'] = 'VerknÃ¼pft mit den folgenden bayesschen Dokumenten';
+$gtext['max_filesize'] = 'Maximale DateigrÃ¶Ãen';
+$gtext['month'] = 'Monat';
+$gtext['new'] = 'Neuer Blog';
+$gtext['no_comments'] = 'Keine Kommentare';
+$gtext['not_found'] = 'Nicht gefunden';
+$gtext['on'] = 'Auf';
+$gtext['p_link'] = 'Permanente VerknÃ¼pfung';
+$gtext['posted_blog'] = 'Neuen Blog verÃ¶ffentlicht mit Titel';
+$gtext['published'] = 'VerÃ¶ffentlicht';
+$gtext['recent_comments'] = 'Aktuelle Kommentare';
+$gtext['replied_blog'] = 'Auf einen Blog geantwortet mit Titel';
+$gtext['reply'] = 'Antworten';
+$gtext['save_draft'] = 'Als Entwurf speichern';
+$gtext['submit'] = 'Senden';
+$gtext['suggest_bookmarks'] = 'Lesezeichen vorschlagen';
+$gtext['tag'] = 'Tag';
+$gtext['tag_cloud'] = 'Tag-Cloud';
+$gtext['tags'] = 'Tags';
+$gtext['tags_2'] = 'Tags, Kommando getrennt';
+$gtext['thread'] = 'Thread';
+$gtext['time'] = 'Zeit';
+$gtext['title'] = 'Titel';
+$gtext['unset_image'] = 'Aktuelles Bild nicht (mehr) gesetzt';
+$gtext['url'] = 'URL';
+
+?>
