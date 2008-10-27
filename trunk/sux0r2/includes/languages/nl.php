372e979efdb5fcc2e48456c7c48d1d5f3dd61e85
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-10-27 18:15:32 +0000

    Dutch translation by Peter van Es

---
+++ trunk/sux0r2/includes/languages/nl.php
@@ -0,0 +1,43 @@
+<?php
+
+/* Navigation menu */
+
+$gtext['navcontainer'] = array(
+
+    'Home' => suxFunct::makeUrl('/home'),
+    'Blog' => suxFunct::makeUrl('/blog'),
+    'Feeds' => suxFunct::makeUrl('/feeds'),
+    'Favorieten' => suxFunct::makeUrl('/bookmarks'),
+    'Fotos' => suxFunct::makeUrl('/photos'),
+    'Source Code' => 'http://sourceforge.net/projects/sux0r/',
+    'Doneer!' => 'http://sourceforge.net/project/project_donations.php?group_id=131752',
+
+    );
+
+/* Copyright */
+
+$gtext['copyright'] = '<a href="http://www.sux0r.org/">sux0r</a> is copyright &copy;
+<a href="http://www.trotch.com/">Trotch Inc</a> ' . date('Y') . ' en is uitgebracht onder
+de <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero General Public License</a>.
+Hosting door <a href="http://www.networkredux.com/">Network Redux</a>.';
+
+$gtext['data_license'] = 'Tenzij anders vermeld, is alle inhoud op deze site is copyright bij de auteur en beschikbaar onder de  <br />
+<a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0</a> .
+Auteurs worden bij hun volledige naam of nickname genoemd.';
+
+/* Now back our regular scheduled program */
+
+$gtext['404_continue'] = 'Klik hier om door te gaan';
+$gtext['404_h1'] = 'Oops, Pagina niet gevonden (Error 404)';
+$gtext['404_p1'] = 'Door een bepaalde reden (verkeerde URL, foute link vanaf een andere site, verouderde zoekmachine index of we hebben de file verwijderd) is de pagina die je zoekt niet beschikbaar.';
+$gtext['admin'] = 'Administratie';
+$gtext['banned_continue'] = 'Klik hier om door te gaan';
+$gtext['banned_h1'] = 'Verbannen';
+$gtext['banned_p1'] = 'Je bent een slecht persoon geweest, een heel slecht persoon.';
+$gtext['continue'] = 'Doorgaan';
+$gtext['login'] = 'Inloggen';
+$gtext['logout'] = 'Uitloggen';
+$gtext['register'] = 'Registreren';
+$gtext['welcome'] = 'Welkom';
+
+?>
---
+++ trunk/sux0r2/modules/blog/languages/nl.php
@@ -0,0 +1,65 @@
+<?php
+
+// Always UTF-8
+// Always uppercase the first word
+
+$gtext['admin'] = 'Blog Administratie';
+$gtext['alert_delete'] = 'Weet je zeker dat je deze blog wilt verwijderen? Reacties zullen ook worden verwijderd.';
+$gtext['archives'] = 'Archieven';
+$gtext['author'] = 'Auteur';
+$gtext['authors'] = 'Auteurs';
+$gtext['back'] = 'Terug naar Blog';
+$gtext['bayes_categories'] = 'Bayesian CategorieÃ«n Door Auteur';
+$gtext['blog'] = 'Blog';
+$gtext['body'] = 'Bericht';
+$gtext['bookmarks'] = 'Favorieten';
+$gtext['by'] = 'Door';
+$gtext['cancel'] = 'Annuleren';
+$gtext['category'] = 'Categorie';
+$gtext['comments'] = 'Reacties';
+$gtext['date'] = 'Datum';
+$gtext['delete'] = 'Verwijderen';
+$gtext['draft'] = 'Klad';
+$gtext['edit'] = 'Aanpassen';
+$gtext['edit_2'] = 'Blog Aanpassen';
+$gtext['error_1'] = 'Titel mag niet leeg zijn';
+$gtext['error_2'] = 'Bericht mag niet leeg zijn';
+$gtext['error_3'] = 'Ongeldig bestands type';
+$gtext['error_4'] = 'Bestand is te groot';
+$gtext['error_5'] = 'Ongeldige datum';
+$gtext['error_6'] = 'Ongeldige tijd';
+$gtext['error_7'] = 'URL is leeg';
+$gtext['error_8'] = 'URL is onjuist';
+$gtext['extensions'] = 'Ondersteunde extensies';
+$gtext['form_error'] = 'Het formulier is niet verstuurd, zie onderstaande error(s)';
+$gtext['form_problem'] = 'Het formulier is niet verstuurd, probeer het nogmaals';
+$gtext['header'] = 'Het strijken van onze broek in het heelal ';
+$gtext['image'] = 'Afbeelding';
+$gtext['in'] = 'In';
+$gtext['linked_to'] = 'Gelinkt aan de volgende Bayes documenten';
+$gtext['max_filesize'] = 'Maximum bestands grootte';
+$gtext['month'] = 'Maand';
+$gtext['new'] = 'Nieuwe Blog';
+$gtext['no_comments'] = 'Geen Reacties';
+$gtext['not_found'] = 'Niet gevonden';
+$gtext['on'] = 'On';
+$gtext['p_link'] = 'Permanente Link';
+$gtext['posted_blog'] = 'Publiseerd een nieuwe blog getiteld';
+$gtext['published'] = 'Gepubliseerd';
+$gtext['recent_comments'] = 'Recente reacties';
+$gtext['replied_blog'] = 'Reageert op een blog getiteld';
+$gtext['reply'] = 'Beantwoorden';
+$gtext['save_draft'] = 'Opslaan als klad';
+$gtext['submit'] = 'Versturen';
+$gtext['suggest_bookmarks'] = 'Aanbevolen favorieten';
+$gtext['tag'] = 'Tag';
+$gtext['tag_cloud'] = 'Tag cloud';
+$gtext['tags'] = 'Tags';
+$gtext['tags_2'] = 'Tags, commando afgebroken';
+$gtext['thread'] = 'Thread';
+$gtext['time'] = 'Tijd';
+$gtext['title'] = 'Titel';
+$gtext['unset_image'] = 'Unset huidige afbeelding';
+$gtext['url'] = 'URL';
+
+?>
