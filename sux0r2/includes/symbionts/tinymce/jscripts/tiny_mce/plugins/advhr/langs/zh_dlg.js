5b75c50c6433fb369280dbeedd42c5fdcf9623b6
Dac Chartrand <connerbw@users.noreply.github.com>
2008-11-21 17:02:10 +0000

    Updated to TinyMCE 3.2.1

--- sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/plugins/advhr/langs/zh_dlg.js
+++ sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/plugins/advhr/langs/zh_dlg.js
@@ -1,5 +1,5 @@
 tinyMCE.addI18n('zh.advhr_dlg',{
-width:"\u5BBD\u5EA6",
-size:"\u9AD8\u5EA6",
-noshade:"\u65E0\u9634\u5F71"
+width:" \r\n\u5BBD \r\n",
+size:" \r\n\u957F \r\n",
+noshade:" \r\n\u65E0\u9634\u5F71 \r\n"
 });
--- sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/themes/simple/langs/tw.js
+++ sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/themes/simple/langs/tw.js
@@ -1,11 +1,11 @@
 tinyMCE.addI18n('tw.simple',{
 bold_desc:"\u7C97\u9AD4(Ctrl+B)",
 italic_desc:"\u659C\u9AD4(Ctrl+I)",
-underline_desc:"\u5E95\u7DDA (Ctrl+U)",
+underline_desc:"\u4E0B\u5283\u7DDA(Ctrl+U)",
 striketrough_desc:"\u522A\u9664\u7DDA",
-bullist_desc:"\u6E05\u55AE\u7B26\u865F",
-numlist_desc:"\u7DE8\u865F",
-undo_desc:"\u5FA9\u539F (Ctrl+Z)",
-redo_desc:"\u53D6\u6D88\u5FA9\u539F (Ctrl+Y)",
-cleanup_desc:"\u522A\u9664\u5197\u9918\u78BC"
+bullist_desc:"\u9805\u76EE\u5217\u8868",
+numlist_desc:"\u7DE8\u865F\u5217\u8868",
+undo_desc:"\u64A4\u92B7(Ctrl+Z)",
+redo_desc:"\u91CD\u505A(Ctrl+Y)",
+cleanup_desc:"\u6E05\u9664\u6A19\u7C64"
 });
