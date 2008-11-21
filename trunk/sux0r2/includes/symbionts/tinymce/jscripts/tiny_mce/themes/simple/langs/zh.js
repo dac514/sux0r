93ad79be8db387ac6e7da0cffa718d57853d9d16
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2008-11-21 17:02:10 +0000

    Updated to TinyMCE 3.2.1

--- trunk/sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/themes/simple/langs/zh.js
+++ trunk/sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/themes/simple/langs/zh.js
@@ -1,11 +1,11 @@
 tinyMCE.addI18n('zh.simple',{
-bold_desc:"\u7C97\u4F53(Ctrl+B)",
-italic_desc:"\u659C\u4F53(Ctrl+I)",
-underline_desc:"\u4E0B\u5212\u7EBF(Ctrl+U)",
-striketrough_desc:"\u5220\u9664\u7EBF",
-bullist_desc:"\u9879\u76EE\u5217\u8868",
-numlist_desc:"\u7F16\u53F7\u5217\u8868",
-undo_desc:"\u64A4\u9500(Ctrl+Z)",
-redo_desc:"\u91CD\u505A(Ctrl+Y)",
-cleanup_desc:"\u6E05\u9664\u6807\u7B7E"
+bold_desc:" \r\n\u9ED1\u4F53(Ctrl+B) \r\n",
+italic_desc:" \r\n\u659C\u4F53(Ctrl+I) \r\n",
+underline_desc:" \r\n\u5E95\u7EBF(Ctrl+U) \r\n",
+striketrough_desc:" \r\n\u5220\u9664\u7EBF \r\n",
+bullist_desc:" \r\n\u9879\u76EE\u7B26\u53F7 \r\n",
+numlist_desc:" \r\n\u7F16\u53F7 \r\n",
+undo_desc:" \r\n\u590D\u539F(Ctrl+Z) \r\n",
+redo_desc:" \r\n\u91CD\u505A(Ctrl+Y) \r\n",
+cleanup_desc:" \r\n\u6E05\u9664\u591A\u4F59\u4EE3\u7801 \r\n"
 });
