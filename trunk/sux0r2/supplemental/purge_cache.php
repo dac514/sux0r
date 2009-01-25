5c849acf0fba1c1b389eebf2ae63f71f20c655df
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2009-01-25 21:24:24 +0000

    Purge templates_c while you are at it

--- trunk/sux0r2/supplemental/purge_cache.php
+++ trunk/sux0r2/supplemental/purge_cache.php
@@ -6,10 +6,11 @@ require_once(dirname(__FILE__)  . '/../includes/suxFunct.php'); // Configuration
 $cache = array();
 $cache[] = realpath(dirname(__FILE__) . '/../temporary/cache/');
 $cache[] = realpath(dirname(__FILE__) . '/../temporary/rss_cache/');
+$cache[] = realpath(dirname(__FILE__) . '/../temporary/templates_c/');
 
 // Go!
 foreach($cache as $dir) {
-    suxFunct::obliterateDir($dir);  
+    suxFunct::obliterateDir($dir);
 }
 
 ?>
