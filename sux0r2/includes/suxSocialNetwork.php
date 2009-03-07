50ae16f69033f0e4a74fdbb63ac4b8e01b09173d
Dac Chartrand <connerbw@users.noreply.github.com>
2009-03-07 15:48:18 +0000

    Removed header comments; I want to avoid having to edit files if/when license changes happen.

--- sux0r2/includes/suxSocialNetwork.php
+++ sux0r2/includes/suxSocialNetwork.php
@@ -3,23 +3,8 @@
 /**
 * suxSocialNetwork
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
 * @author     Dac Chartrand <dac.chartrand@gmail.com>
-* @copyright  2008 sux0r development group
 * @license    http://www.gnu.org/licenses/agpl.html
-*
 */
 
 // Based on version 1.1 of the XFN meta data profile
