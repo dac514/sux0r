354b1af5684c563067051ba401f8739d999a0966
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2009-03-07 15:48:18 +0000

    Removed header comments; I want to avoid having to edit files if/when license changes happen.

--- trunk/sux0r2/modules/openid/openid.php
+++ trunk/sux0r2/modules/openid/openid.php
@@ -3,26 +3,11 @@
 /**
 * openid
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
 * Forked From / Inspired by:
 * CJ Niemira: http://siege.org/projects/phpMyID/
 *
 * @author     Dac Chartrand <dac.chartrand@gmail.com>
-* @copyright  2008 sux0r development group
 * @license    http://www.gnu.org/licenses/agpl.html
-*
 */
 
 require_once(dirname(__FILE__) . '/../../includes/suxTemplate.php');
