0de333042629a5fe8dd088b9012c05657afde624
Dac Chartrand <connerbw@users.noreply.github.com>
2009-05-19 02:36:57 +0000

    MySql: Changed MyISAM tables to InnoDB tables, Postgres: renamed constraints.

--- sux0r2/supplemental/sql/upgrade-205_to_206-mysql.sql
+++ sux0r2/supplemental/sql/upgrade-205_to_206-mysql.sql
@@ -4,4 +4,12 @@ RENAME TABLE link_bayes_rss TO link__bayes_documents__rss_items;
 RENAME TABLE link_bookmarks_tags TO link__bookmarks__tags;
 RENAME TABLE link_bookmarks_users TO link__bookmarks__users;
 RENAME TABLE link_messages_tags TO link__messages__tags;
-RENAME TABLE link_rss_users TO link__rss_feeds__users; 
+RENAME TABLE link_rss_users TO link__rss_feeds__users;
+
+ALTER TABLE bayes_cache ENGINE = innodb;
+ALTER TABLE bookmarks ENGINE = innodb;
+ALTER TABLE openid_secrets ENGINE = innodb;
+ALTER TABLE openid_trusted ENGINE = innodb;
+ALTER TABLE socialnetwork ENGINE = innodb;
+ALTER TABLE tags ENGINE = innodb;
+ALTER TABLE users_log ENGINE = innodb;
--- sux0r2/supplemental/sql/upgrade-205_to_206-postgres.sql
+++ sux0r2/supplemental/sql/upgrade-205_to_206-postgres.sql
@@ -4,4 +4,27 @@ ALTER TABLE link_bayes_rss RENAME TO link__bayes_documents__rss_items;
 ALTER TABLE link_bookmarks_tags RENAME TO link__bookmarks__tags;
 ALTER TABLE link_bookmarks_users RENAME TO link__bookmarks__users;
 ALTER TABLE link_messages_tags RENAME TO link__messages__tags;
-ALTER TABLE link_rss_users RENAME TO link__rss_feeds__users; 
+ALTER TABLE link_rss_users RENAME TO link__rss_feeds__users;
+
+
+ALTER TABLE link__bayes_documents__bookmarks DROP CONSTRAINT link_bayes_bookmarks_grouping;
+ALTER TABLE link__bayes_documents__bookmarks ADD CONSTRAINT link__bayes_documents__bookmarks_grouping UNIQUE (bookmarks_id,bayes_documents_id);
+
+ALTER TABLE link__bayes_documents__messages DROP CONSTRAINT link_bayes_messages_grouping;
+ALTER TABLE link__bayes_documents__messages ADD CONSTRAINT link__bayes_documents__messages_grouping UNIQUE (messages_id, bayes_documents_id);
+
+ALTER TABLE link__bayes_documents__rss_items DROP CONSTRAINT link_bayes_rss_grouping;
+ALTER TABLE link__bayes_documents__rss_items ADD CONSTRAINT link__bayes_documents__rss_items_grouping UNIQUE (rss_items_id, bayes_documents_id);
+
+ALTER TABLE link__bookmarks__tags DROP CONSTRAINT link_bookmarks_tags_grouping;
+ALTER TABLE link__bookmarks__tags ADD CONSTRAINT link__bookmarks__tags_grouping UNIQUE (bookmarks_id, tags_id);
+
+
+ALTER TABLE link__bookmarks__users DROP CONSTRAINT link_bookmarks_users_grouping;
+ALTER TABLE link__bookmarks__users ADD CONSTRAINT link__bookmarks__users_grouping UNIQUE (bookmarks_id, users_id);
+
+ALTER TABLE link__messages__tags DROP CONSTRAINT link_messages_tags_grouping;
+ALTER TABLE link__messages__tags ADD CONSTRAINT link__messages__tags_grouping UNIQUE (messages_id, tags_id);
+
+ALTER TABLE link__rss_feeds__users DROP CONSTRAINT link_rss_users_grouping;
+ALTER TABLE link__rss_feeds__users ADD CONSTRAINT link__rss_feeds__users_grouping UNIQUE (rss_feeds_id, users_id);
