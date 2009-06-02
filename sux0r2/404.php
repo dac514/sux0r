c8f40866a17e6c2adb129a409c4fcfc180aa04bd
Dac Chartrand <connerbw@users.noreply.github.com>
2009-06-02 21:08:29 +0000

    Discourage inline HTML, checkpoint for MySQL optimizations.

--- sux0r2/404.php
+++ sux0r2/404.php
@@ -4,6 +4,10 @@ require_once(dirname(__FILE__) . '/config.php'); // Configuration
 require_once(dirname(__FILE__) . '/initialize.php'); // Initialization
 $text = suxFunct::gtext(); // Language
 
+// ---------------------------------------------------------------------------
+// Inline, horrible inline
+// ---------------------------------------------------------------------------
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
