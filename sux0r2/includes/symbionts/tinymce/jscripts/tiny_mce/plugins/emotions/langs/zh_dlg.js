14e9d0f85f757de5de599067a8cc8feb3c6647d3
Dac Chartrand <connerbw@users.noreply.github.com>
2009-06-07 22:17:58 +0000

    Updated to TinyMCE 3.2.4.1

--- sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/plugins/emotions/langs/zh_dlg.js
+++ sux0r2/includes/symbionts/tinymce/jscripts/tiny_mce/plugins/emotions/langs/zh_dlg.js
@@ -1,6 +1,6 @@
 tinyMCE.addI18n('zh.emotions_dlg',{
-title:"\u63D2\u5165\u8868\u60C5\u7B26\u53F7",
-desc:"\u8868\u60C5\u7B26\u53F7",
+title:"\u63D2\u5165\u56FE\u91CA",
+desc:"\u56FE\u91CA",
 cool:"\u51B7\u9177",
 cry:"\u54ED\u6CE3",
 embarassed:"\u5C34\u5C2C",
