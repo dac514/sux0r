b6ee774918a2d6ab5c9e9943b759a0146e9025a9
Dac Chartrand <connerbw@users.noreply.github.com>
2010-05-27 17:55:01 +0000

    Checkpoint, working on Web PI stuff

--- sux0r2/supplemental/iis_web_pi/package.sh
+++ sux0r2/supplemental/iis_web_pi/package.sh
@@ -10,6 +10,7 @@ mv sux0r/supplemental/iis_web_pi/parameters.xml .
 mv sux0r/supplemental/iis_web_pi/sux0r/web.config sux0r
 mv sux0r/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql sux0r/supplemental/sql
 mv sux0r/supplemental/iis_web_pi/sux0r/documentation/README-IIS.txt sux0r/documentation
+mv sux0r/supplemental/iis_web_pi/sux0r/templates/sux0r/home/home.tpl templates/sux0r/home
 rm -rf sux0r/supplemental/iis_web_pi
 zip -r sux0r.zip .
 mv sux0r.zip ~/Desktop
