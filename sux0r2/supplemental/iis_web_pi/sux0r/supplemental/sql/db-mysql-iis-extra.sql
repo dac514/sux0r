87a76d5e8f63457ed9e0c039488f1058eceedac7
Dac Chartrand <connerbw@users.noreply.github.com>
2010-05-27 16:39:07 +0000

    Checkpoint, working on Web PI stuff

---
+++ sux0r2/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (nickname,email,password,root,banned) VALUES ('PlaceholderForAdmin','PlaceholderForAdminEmail',MD5('PlaceholderForAdmin:PlaceholderForRealm:PlaceholderForAdminPassword'),1,null);
