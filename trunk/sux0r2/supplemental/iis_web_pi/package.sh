3d473995c7bab9964ab32a4ffcd8dbce0de33e08
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2010-05-27 16:52:22 +0000

    Checkpoint, working on Web PI stuff

--- trunk/sux0r2/supplemental/iis_web_pi/package.sh
+++ trunk/sux0r2/supplemental/iis_web_pi/package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -o verbose
 mkdir /tmp/suxpack
 cd /tmp/suxpack
 svn export https://sux0r.svn.sourceforge.net/svnroot/sux0r/trunk/sux0r2 sux0r
@@ -8,4 +9,8 @@ mv sux0r/supplemental/iis_web_pi/manifest.xml .
 mv sux0r/supplemental/iis_web_pi/parameters.xml .
 mv sux0r/supplemental/iis_web_pi/sux0r/web.config sux0r
 mv sux0r/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql sux0r/supplemental/sql
+mv sux0r/supplemental/iis_web_pi/sux0r/documentation/README-IIS.txt sux0r/documentation
 rm -rf sux0r/supplemental/iis_web_pi
+zip -r sux0r.zip .
+mv sux0r.zip ~/Desktop
+rm -rf /tmp/suxpack
