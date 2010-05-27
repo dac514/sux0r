242436330f629c74419987ec69862ae602ee7f76
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2010-05-27 20:35:28 +0000

    Bugfixes for Web PI

--- trunk/sux0r2/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql
+++ trunk/sux0r2/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql
@@ -1,1 +1,3 @@
+USE PlaceholderForDbName;
+
 INSERT INTO users (nickname,email,password,root,banned) VALUES ('PlaceholderForAdminUser','PlaceholderForAdminEmail',MD5('PlaceholderForAdminUser:PlaceholderForRealm:PlaceholderForAdminPassword'),1,null);
