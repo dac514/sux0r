994cee9f0e8853664e9163e82a9d1d4fafbcec19
conner_bw <conner_bw@ba47ce49-cc11-47aa-8923-2de552c162d1>
2011-03-03 03:15:57 +0000

    Improved packaging script.

--- trunk/sux0r2/supplemental/iis_web_pi/package.sh
+++ trunk/sux0r2/supplemental/iis_web_pi/package.sh
@@ -4,6 +4,8 @@ set -o verbose
 mkdir /tmp/suxpack
 cd /tmp/suxpack
 svn export https://sux0r.svn.sourceforge.net/svnroot/sux0r/trunk/sux0r2 sux0r
+zip -r sux0r-x.x.x.zip .
+mv sux0r-x.x.x.zip ~/Desktop
 mv sux0r/supplemental/iis_web_pi/install.sql .
 mv sux0r/supplemental/iis_web_pi/manifest.xml .
 mv sux0r/supplemental/iis_web_pi/parameters.xml .
@@ -12,6 +14,6 @@ mv sux0r/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql s
 mv sux0r/supplemental/iis_web_pi/sux0r/documentation/README-IIS.txt sux0r/documentation
 mv sux0r/supplemental/iis_web_pi/sux0r/templates/sux0r/home/home.tpl sux0r/templates/sux0r/home
 rm -rf sux0r/supplemental/iis_web_pi
-zip -r sux0r.zip .
-mv sux0r.zip ~/Desktop
+zip -r sux0r-x.x.x-IIS.zip .
+mv sux0r-x.x.x-IIS.zip ~/Desktop
 rm -rf /tmp/suxpack
