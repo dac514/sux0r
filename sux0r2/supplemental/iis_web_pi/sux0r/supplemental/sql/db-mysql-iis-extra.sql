0096e1d327bafb02f0b4f010a090485841da1d15
Dac Chartrand <connerbw@users.noreply.github.com>
2013-04-25 17:28:49 +0000

    Removed iis_web_pi directory, no longer maintained.

--- sux0r2/supplemental/iis_web_pi/sux0r/supplemental/sql/db-mysql-iis-extra.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (nickname,email,password,root,banned) VALUES ('PlaceholderForAdminUser','PlaceholderForAdminEmail',MD5('PlaceholderForAdminUser:PlaceholderForRealm:PlaceholderForAdminPassword'),1,null);
